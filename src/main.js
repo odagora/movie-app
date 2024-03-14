304d68c7fde57c8bc0b13d9a6925cfb0dbd0e06b
Daniel <oscar.d.gonzalez@gmail.com>
2024-03-14 16:47:03 -0500

    feat: :sparkles: added trending movies preview feature

--- src/main.js
+++ src/main.js
@@ -1,1 +1,26 @@
-console.log(`API URL= https://example.com/hi/?api_key=${API_KEY}`);
+const BASE_URL = 'https://api.themoviedb.org/3';
+const BASE_IMAGE_URL = 'https://image.tmdb.org/t/p/w300';
+
+async function getTrendingMoviesPreview() {
+    const res = await fetch(`${BASE_URL}/trending/movie/day?api_key=${API_KEY}`);
+    const data = await res.json();
+    const movies = data.results;
+
+    const movieTrendingPreviewContainer = document.querySelector('.trendingPreview-movieList')
+    const moviesList = []
+    movies.forEach(movie => {
+      const movieContainer = document.createElement('div');
+      const movieImage = document.createElement('img');
+
+      movieContainer.classList.add('movie-container');
+      movieImage.classList.add('movie-img');
+      movieImage.setAttribute('alt', movie.title);
+      movieImage.setAttribute('src', `${BASE_IMAGE_URL}/${movie.poster_path}`);
+      movieContainer.appendChild(movieImage);
+      moviesList.push(movieContainer);
+    })
+
+    movieTrendingPreviewContainer.append(...moviesList)
+}
+
+getTrendingMoviesPreview();
